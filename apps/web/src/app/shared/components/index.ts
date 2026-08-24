0433cfe3357997e93562c0efe2ff5f077fbcce55
Vìctor J. <maximovj@outlook.com>
2026-08-24 01:13:27 -0600

    * Se eliminan componentes inncesarias

--- apps/web/src/app/shared/components/index.ts
+++ apps/web/src/app/shared/components/index.ts
@@ -1,6 +1,3 @@
-export { MainContentComponent } from "./main-content/main-content.component";
-export { SidebarComponent } from "./sidebar/sidebar.component";
-
 export { TopHeaderComponent } from "./top-header/top-header.component";
 export { NavVerticalComponent } from "./nav-vertical/nav-vertical.component";
 export { ContentComponent } from "./content/content.component";
