5a98c9b2ae55a9bd9dec0a5813db899358d6ffb9
Vìctor J. <maximovj@outlook.com>
2026-08-24 02:27:05 -0600

    * Se modifica contenido de dashboard

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyCounterButtonComponent } from '@repo/ui';
 import { TopHeaderComponent, NavVerticalComponent } from './shared/components';
-import { TabEventosComponent } from './features';
+import { TabEventosComponent, TabDashboardComponent } from './features';
 import { Tab } from './shared/enum/Tab.enum';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TopHeaderComponent, NavVerticalComponent, TabEventosComponent],
+  imports: [
+    TopHeaderComponent, 
+    NavVerticalComponent, 
+    TabDashboardComponent,
+    TabEventosComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- apps/web/src/app/features/tab-dashboard/tab-dashboard.component.ts
+++ apps/web/src/app/features/tab-dashboard/tab-dashboard.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContentComponent } from '../../shared/components';
 
 @Component({
   selector: 'app-tab-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ContentComponent],
   templateUrl: './tab-dashboard.component.html',
   styleUrl: './tab-dashboard.component.css'
 })
