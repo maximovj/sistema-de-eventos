5aae721286636fb93c8327f63b0b2af173aeafbb
Vìctor J. <maximovj@outlook.com>
2026-08-24 02:24:03 -0600

    * Se agrega nuevos Tab

--- apps/web/src/app/shared/enum/Tab.enum.ts
+++ apps/web/src/app/shared/enum/Tab.enum.ts
@@ -4,6 +4,13 @@ export class Tab {
     public static TabSedes = new Tab(1, "tab-sedes");
     public static TabArtistas = new Tab(1, "tab-artistas");
     public static TabActividades = new Tab(1, "tab-actividades");
+    public static TabVentas = new Tab(1, "tab-ventas");
+    public static TabProveedores= new Tab(1, "tab-proveedores");
+    public static TabPatrocinadores = new Tab(1, "tab-patrocinadores");
+    public static TabGastos = new Tab(1, "tab-gastos");
+    public static TabStaff = new Tab(1, "tab-staff");
+    public static TabIncidencias = new Tab(1, "tab-incidencias");
+    public static TabConfiguracion = new Tab(1, "tab-configuracion");
 
     constructor(
         public id: number,
@@ -17,6 +24,13 @@ export class Tab {
             Tab.TabSedes,
             Tab.TabArtistas,
             Tab.TabActividades,
+            Tab.TabVentas,
+            Tab.TabProveedores,
+            Tab.TabPatrocinadores,
+            Tab.TabGastos,
+            Tab.TabStaff,
+            Tab.TabIncidencias,
+            Tab.TabConfiguracion,
         ];
     }
 }
