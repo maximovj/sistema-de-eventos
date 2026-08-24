7f3fd03e06aa20bc60bc605bafc6401f4c908fc3
Vìctor J. <maximovj@outlook.com>
2026-08-24 02:15:15 -0600

    * Se agrega funcionalidad para cambiar estilos / clases al activar una pestaña

--- apps/web/src/app/shared/components/nav-vertical/nav-vertical.component.ts
+++ apps/web/src/app/shared/components/nav-vertical/nav-vertical.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Tab } from '../../enum/Tab.enum';
 
 @Component({
@@ -9,13 +9,17 @@ import { Tab } from '../../enum/Tab.enum';
   styleUrl: './nav-vertical.component.css'
 })
 export class NavVerticalComponent {
+  @Input() tabActive!:Tab;
   @Output() onChangeTab: EventEmitter<Tab> = new EventEmitter<Tab>();
   tabs = Tab;
-  tabActive: string = 'dashboard';
 
   changeTab(tab: Tab) {
     sessionStorage.setItem('tab', tab.etiqueta);
     this.onChangeTab.emit(tab);
+  }
+
+  isActive(tab: Tab): string {
+    return this.tabActive == tab ? 'nav-item active' : 'nav-item';
   }
 
 }
