130b2af75a8cf49938124ea0e728f9aa2ed888f3
Vìctor J. <maximovj@outlook.com>
2026-09-04 21:07:50 -0600

    web: Se crea integra componente tab-organizadores en app

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -2,19 +2,19 @@ import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SettingsStorageService } from '@repo/shared-services';
 import { TopHeaderComponent, NavVerticalComponent } from './shared/components';
-import { TabEventosComponent, TabDashboardComponent, TabPredeterminadoComponent } from './features';
+import { TabEventosComponent, TabDashboardComponent, TabPredeterminadoComponent, TabOrganizadoresComponent } from './features';
 import { Tab } from '@repo/shared-types';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    TopHeaderComponent, 
-    NavVerticalComponent, 
+    TopHeaderComponent,
+    NavVerticalComponent,
     TabDashboardComponent,
     TabEventosComponent,
     TabPredeterminadoComponent,
-  ],
+    TabOrganizadoresComponent,
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
