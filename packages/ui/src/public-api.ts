1b1601b9d41e1ef8e5648fe34706b1700147d583
Víctor J. <maximovj@outlook.com>
2026-09-06 19:15:08 -0600

    package/ui: Se exporta nuevos componentes

--- packages/ui/src/public-api.ts
+++ packages/ui/src/public-api.ts
@@ -7,9 +7,12 @@ export * from './ui/components/shared/spinner/spinner.component';
 export * from './ui/components/shared/stat-box/stat-box.component';
 export * from './ui/components/shared/toast/toast-container.component';
 export * from './ui/components/layout/modal-layout/modal-layout.component';
+export * from './ui/components/layout/pagination-layout/pagination-layout.component';
+export * from './ui/components/layout/table-layout/table-layout.component';
 
 // Tab Organizador
 export * from './ui/components/features/tab-organizadores/modal-organizador/modal-organizador.component';
+export * from './ui/components/features/tab-organizadores/fila-organizador/fila-organizador.component';
 
 // Tab Eventos
 export * from './ui/components/features/tab-eventos/event-card/event-card.component';
