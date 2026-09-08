02d904aefa4d4920c5ee153d512ceeb319636b83
Víctor J. <maximovj@outlook.com>
2026-09-07 20:49:30 -0600

    packages/web: Se agrega tab sedes en app componente

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { SettingsStorageService } from '@repo/shared-services';
 import { ToastContainerComponent } from '@repo/ui';
 import { TopHeaderComponent, NavVerticalComponent } from './shared/components';
-import { TabEventosComponent, TabDashboardComponent, TabPredeterminadoComponent, TabOrganizadoresComponent } from './features';
+import { TabEventosComponent, TabDashboardComponent, TabPredeterminadoComponent, TabOrganizadoresComponent, TabSedesComponent } from './features';
 import { Tab } from '@repo/shared-types';
 @Component({
   selector: 'app-root',
@@ -16,6 +16,7 @@ import { Tab } from '@repo/shared-types';
     TabEventosComponent,
     TabPredeterminadoComponent,
     TabOrganizadoresComponent,
+    TabSedesComponent,
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
