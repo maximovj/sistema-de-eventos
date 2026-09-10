ac20acc226f5b799c462c584ba065b054c5ffb41
Víctor J. <maximovj@outlook.com>
2026-09-09 21:06:26 -0600

    apps/web: Se usa/ Se implementa servicios para artistas en componente de tab artistas

--- apps/web/src/app/features/tab-artistas/tab-artistas.component.ts
+++ apps/web/src/app/features/tab-artistas/tab-artistas.component.ts
@@ -1,17 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { TabContentComponent } from "../../shared/components";
 import { CommonModule } from '@angular/common';
+import { ArtistasService } from '@repo/shared-services';
+import { Artista } from '@repo/shared-types';
+import { delay, finalize } from 'rxjs';
+import { SpinnerComponent } from '@repo/ui';
 
 @Component({
   selector: 'app-tab-artistas',
   standalone: true,
   imports: [
     CommonModule,
+    SpinnerComponent,
     TabContentComponent
   ],
   templateUrl: './tab-artistas.component.html',
   styleUrl: './tab-artistas.component.css'
 })
-export class TabArtistasComponent {
+export class TabArtistasComponent implements OnInit {
+  private service = inject(ArtistasService);
+
+  // Estados signals
+  cargando = signal<boolean>(false);
+  artistas = signal<Artista[]>([]);
+
+  // Estados computadas
+  total = computed(() => this.artistas().length || 0);
+  
+  ngOnInit(): void {
+    this.cargarArtistas();
+  }
+
+  cargarArtistas() {
+    this.cargando.set(true);
+    this.service
+    .artistas()
+    .pipe(
+      delay(1700),
+      finalize(() => this.cargando.set(false))
+    )
+    .subscribe({
+      next: (value) => {
+        this.artistas.set(value);
+      },
+    });
+  }
+
 
 }
