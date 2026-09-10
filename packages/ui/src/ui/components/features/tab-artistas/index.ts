dff1fa285d50cd0d829ec7239a878441f2e57dfa
Víctor J. <maximovj@outlook.com>
2026-09-09 23:17:22 -0600

    apps/ui: Se exporta componente grid tarjetas para tab artistas

--- packages/ui/src/ui/components/features/tab-artistas/index.ts
+++ packages/ui/src/ui/components/features/tab-artistas/index.ts
@@ -1,2 +1,3 @@
 export * from './stats-artistas/stats-artistas.component';
 export * from './filtros-artistas/filtros-artistas.component';
+export * from './grid-tarjetas-artistas/grid-tarjetas-artistas.component';
