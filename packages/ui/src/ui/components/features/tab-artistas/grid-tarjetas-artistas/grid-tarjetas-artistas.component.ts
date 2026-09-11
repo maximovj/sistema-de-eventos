5a53b6de8c024cb255f34c546e2677e462c5132b
Víctor J. <maximovj@outlook.com>
2026-09-10 22:54:46 -0600

    packages/ui: Se agrega estados de entrada , salida, computadas y funciones auxiliares en grid tarjetas artistas

--- packages/ui/src/ui/components/features/tab-artistas/grid-tarjetas-artistas/grid-tarjetas-artistas.component.ts
+++ packages/ui/src/ui/components/features/tab-artistas/grid-tarjetas-artistas/grid-tarjetas-artistas.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, Input, Output, EventEmitter } from '@angular/core';
+import { Artista } from '@repo/shared-types';
 
 @Component({
   selector: 'ui-grid-tarjetas-artistas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './grid-tarjetas-artistas.component.html',
   styleUrl: './grid-tarjetas-artistas.component.css'
 })
 export class GridTarjetasArtistasComponent {
+  // * Estados / Datos de entrada
+  @Input() cargando!: boolean;
+  @Input() artistas!: Artista[];
+
+  // * Estados / Datos de salida
+  @Output() ver: EventEmitter<Artista> = new EventEmitter<Artista>;
+  @Output() editar: EventEmitter<Artista> = new EventEmitter<Artista>;
+  @Output() eliminar: EventEmitter<Artista> = new EventEmitter<Artista>;
+
+  // * Estados computadas
+  totalArtistas = computed(() => this.artistas.length || 0); 
+  sinArtistas = computed(() => this.artistas.length <= 0);
+
+  // * Funciones auxiliares
+  obtenerIniciales(name: string): string {
+    if (!name) return '';
+    return name
+      .trim()
+      .split(/\s+/)
+      .slice(0, 2)
+      .map(word => word[0].toUpperCase())
+      .join('');
+  }
+
+  obtenerG(nombreArtistico: string): number {
+    const text = nombreArtistico.trim();
+    return text ? text.split(/\s+/).length : 0;
+  }
+
+  verArtista(artista: Artista) {
+    this.ver.emit(artista);
+  }
+
+  editarArtista(artista: Artista) {
+    this.editar.emit(artista);
+  }
+
+  eliminarArtista(artista: Artista) {
+    this.eliminar.emit(artista);
+  }
 
 }
