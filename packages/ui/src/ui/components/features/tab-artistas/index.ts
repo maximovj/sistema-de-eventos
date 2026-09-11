92f4c20281de52fb1e38dd5b1d39aea8bba48c9a
Víctor J. <maximovj@outlook.com>
2026-09-10 23:35:11 -0600

    apps/ui: Se exporta componente modal artistas para tab artistas

--- packages/ui/src/ui/components/features/tab-artistas/index.ts
+++ packages/ui/src/ui/components/features/tab-artistas/index.ts
@@ -3,3 +3,4 @@ export * from './filtros-artistas/filtros-artistas.component';
 export * from './grid-tarjetas-artistas/grid-tarjetas-artistas.component';
 export * from './panel-analitico-artistas/panel-analitico-artistas.component';
 export * from './paginacion-artistas/paginacion-artistas.component';
+export * from './modal-artistas/modal-artistas.component';
