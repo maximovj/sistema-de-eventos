f4c97d06d3c367b7b0021005bb81a199b453f78a
Víctor J. <maximovj@outlook.com>
2026-09-12 20:58:03 -0600

    * Se modifica panel analitico para artistas
    - Se agrega valores y filtros

--- packages/ui/src/ui/components/features/tab-artistas/panel-analitico-artistas/panel-analitico-artistas.component.ts
+++ packages/ui/src/ui/components/features/tab-artistas/panel-analitico-artistas/panel-analitico-artistas.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+import { Artista, ArtistaGeneroMusical, ArtistaTipo } from '@repo/shared-types';
 
 @Component({
   selector: 'ui-panel-analitico-artistas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './panel-analitico-artistas.component.html',
   styleUrl: './panel-analitico-artistas.component.css'
 })
 export class PanelAnaliticoArtistasComponent {
+  // * Estados / Datos de entrada
+  cargando = input<boolean>(false);
+  artistas = input<Artista[]>([]);
+
+  generos = Object.entries(ArtistaGeneroMusical);
+  tipoArtistas = Object.entries(ArtistaTipo);
+
+  top5Artistas = computed(() => {
+    return [...this.artistas()]
+      .sort((a, b) => b.costoTotal - a.costoTotal)
+      .slice(0, 5);
+  });
+
+  contarPorGenero(genero: string) {
+    if(this.artistas().length <= 0) return 0;
+    console.log("this.artistas() => ",this.artistas());
+    return this.artistas().filter(artista => artista.generoMusical === genero).length || 0;
+  }
+
+  contarPorTipoArtista(tipo: string) {
+    if(this.artistas().length <= 0) return 0;
+    return this.artistas().filter(artista => artista.tipoArtista === tipo).length || 0;
+  }
+
+  obtenerIniciales(name: string): string {
+    if (!name) return '';
+    return name
+      .trim()
+      .split(/\s+/)
+      .slice(0, 2)
+      .map(word => word[0].toUpperCase())
+      .join('');
+  }
 
 }
