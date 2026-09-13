3a716ad0382b2c4347e3c9a7caaab444d36f6284
Víctor J. <maximovj@outlook.com>
2026-09-12 20:55:19 -0600

    * Se modifica stats para artistas
    - Se agrega valores y filtros

--- packages/ui/src/ui/components/features/tab-artistas/stats-artistas/stats-artistas.component.ts
+++ packages/ui/src/ui/components/features/tab-artistas/stats-artistas/stats-artistas.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, input, OnChanges, SimpleChanges } from '@angular/core';
+import { Artista, ArtistaEstatusArtista } from '@repo/shared-types';
 
 @Component({
   selector: 'ui-stats-artistas',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './stats-artistas.component.html',
   styleUrl: './stats-artistas.component.css'
 })
-export class StatsArtistasComponent {
+export class StatsArtistasComponent implements OnChanges {
+  
+  // * Estados / Datos de entrada
+  cargando = input<boolean>(false);
+  artistas = input<Artista[]>([]);
+
+  totalArtistas = computed(() => this.artistas().length || 0);
+
+  confirmados = computed(() => this.artistas().filter(item => item.estatus === ArtistaEstatusArtista.CONFIRMADO).length || 0);
+  cancelados = computed(() => this.artistas().filter(item => item.estatus === ArtistaEstatusArtista.CANCELADO).length || 0);
+  pendientes = computed(() => this.artistas().filter(item => item.estatus === ArtistaEstatusArtista.PENDIENTE).length || 0);
+  costoTotal = computed(() => this.artistas().reduce((acc, item) => acc += item.costoTotal, 0));
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['artistas'] && this.artistas) {
+      console.log(this.artistas());
+    }
+  }
 
 }
