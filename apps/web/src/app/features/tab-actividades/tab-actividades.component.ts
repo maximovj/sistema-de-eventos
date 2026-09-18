c6e13a0c77287f9fa4b2370f8f9a75d59b056a19
Víctor J. <maximovj@outlook.com>
2026-09-17 22:54:54 -0600

    app/webs: Se agrega servicio para tab-actividades

--- apps/web/src/app/features/tab-actividades/tab-actividades.component.ts
+++ apps/web/src/app/features/tab-actividades/tab-actividades.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, computed } from '@angular/core';
 import { TabContentComponent } from '../../shared/components';
 import { 
   SpinnerComponent,
@@ -8,6 +8,9 @@ import {
   PaginacionActividadesComponent,
   FiltrosActividadesComponent
 } from '@repo/ui';
+import { ActividadesService } from '@repo/shared-services';
+import { delay, finalize } from 'rxjs';
+import { Actividad } from '@repo/shared-types';
 
 @Component({
   selector: 'app-tab-actividades',
@@ -17,14 +20,40 @@ import {
     TabContentComponent,
     SpinnerComponent,
     StatsActividadesComponent,
+    FiltrosActividadesComponent,
+    GridTarjetasActividadesComponent,
     PaginacionActividadesComponent,
-    GridTarjetasActividadesComponent
 ],
   templateUrl: './tab-actividades.component.html',
   styleUrl: './tab-actividades.component.css'
 })
-export class TabActividadesComponent {
+export class TabActividadesComponent implements OnInit {
+  service = inject(ActividadesService);
 
+  actividades = signal<Actividad[]>([]);
   cargando = signal<boolean>(false);
+
+  // Estados computadas
+  total = computed(() => this.actividades()?.length || 0);
+  
+  public ngOnInit(): void {
+    this.cargandoActividades();
+  }
+
+  private cargandoActividades() {
+    this.cargando.set(true);
+    this.service
+    .actividades()
+    .pipe(
+      delay(1700),
+      finalize(() => this.cargando.set(false)),
+    )
+    .subscribe({
+      next: (value) => {
+        console.log("value => ", value);
+        this.actividades.set(value);
+      },
+    });
+  }
 
 }
