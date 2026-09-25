b602b75f265251caf3c16275dc5d4fe696c711c3
Víctor J. <maximovj@outlook.com>
2026-09-24 21:43:22 -0600

    packages/ui: Se modifica componente  para stats de actividades
    - Se agrega totales

--- packages/ui/src/ui/components/features/tab-actividades/stats-actividades/stats-actividades.component.ts
+++ packages/ui/src/ui/components/features/tab-actividades/stats-actividades/stats-actividades.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { Actividad, ActividadEstatus } from '@repo/shared-types';
 
 @Component({
   selector: 'ui-stats-actividades',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './stats-actividades.component.css'
 })
 export class StatsActividadesComponent {
+  actividades = input<Actividad[]>([]);
+
+  total = computed(() => this.actividades?.length ?? 0);
+
+  totalProceso = computed(() => this.actividades().filter(item => item.estatus === ActividadEstatus.EN_PROGRESO)?.length ?? 0);
+  totalPendientes = computed(() => this.actividades().filter(item => item.estatus === ActividadEstatus.PENDIENTE)?.length ?? 0);
+  totalCompletadas = computed(() => this.actividades().filter(item => item.estatus === ActividadEstatus.COMPLETADA)?.length ?? 0);
 
 }
